7ca7ef331c0a1bc93bb1b9cbc356d4008d3738a1
Albert <rogozinskialbert@gmail.com>
2025-02-19 22:30:57 +0100

    add list users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {//website default
     return view('welcome');
@@ -8,4 +11,5 @@ Route::get('/', function () {//website default
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
