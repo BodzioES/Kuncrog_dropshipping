fe3c29a3c6bff913cd6eb54b27650cd5bab8f6e3
Albert <rogozinskialbert@gmail.com>
2025-04-05 11:08:30 +0200

    crated basic footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,84 +13,94 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script><!--ODPOWIADA MIEDZY INNYMI ZA USUWANIE PRODUKTOW-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+
+<body class="d-flex flex-column min-vh-100">
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto"></ul>
+                <ul class="navbar-nav ms-auto">
+                    @guest
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fa-solid fa-user"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                @if (Route::has('login'))
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                @endif
+                                @if (Route::has('register'))
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                @endif
+                            </div>
+                        </li>
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fa-solid fa-user"></i>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                @can(['isAdmin'])
+                                    <a class="dropdown-item" href="{{ route('users.index') }}">Użytkownicy</a>
+                                    <a class="dropdown-item" href="{{ route('products.index') }}">Produkty</a>
+                                @endcan
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+                <a class="nav-link" href="{{ route('cart.index') }}">
+                    <i class="fa-solid fa-cart-shopping"></i>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="fa-solid fa-user"></i>
-                                </a>
-                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        @if (Route::has('login'))
-                                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                        @endif
-
-                                        @if (Route::has('register'))
-                                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                        @endif
-                                    </div>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="fa-solid fa-user"></i>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    @can(['isAdmin'])
-                                        <a class="dropdown-item" href="{{ route('users.index') }}">Użytkownicy</a>
-                                        <a class="dropdown-item" href="{{ route('products.index') }}">Produkty</a>
-                                    @endcan
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                    <a class="nav-link" href="{{ route('cart.index') }}">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                    </a>
-                </div>
             </div>
-        </nav>
+        </div>
+    </nav>
+    <main class="py-4 flex-grow-1">
+        @yield('content')
+    </main>
+</div>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+<footer class="bg-body-tertiary text-center mt-auto">
+    <div class="container pt-4">
+        <section class="mb-4">
+            <a class="btn btn-link btn-floating btn-lg text-body m-1" href="#"><i class="fab fa-facebook-f"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-body m-1" href="#"><i class="fab fa-twitter"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-body m-1" href="#"><i class="fab fa-google"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-body m-1" href="#"><i class="fab fa-instagram"></i></a>
+            <a class="btn btn-link btn-floating btn-lg text-body m-1" href="#"><i class="fab fa-github"></i></a>
+        </section>
     </div>
-    <script type="text/javascript">
-        @yield('javascript')
-    </script>
-    @yield('js-files')
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2025 Copyright:
+        <a class="text-body" href="#">Kuncrog</a>
+    </div>
+</footer>
+<script type="text/javascript">
+    @yield('javascript')
+</script>
+@yield('js-files')
 </body>
 </html>
+
