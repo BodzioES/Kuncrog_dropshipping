2ead0e479708e76de2e2e130389e2d0cff6aeb25
BodzioES <rogozinskialbert@gmail.com>
2026-09-07 20:28:02 +0200

    fix: TrustProxies null IP error

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,17 +13,13 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = '*'; // ufamy proxy (nginx w Dockerze)
+    protected $proxies = [];
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO;
+    protected $headers = 0;
 
 }
